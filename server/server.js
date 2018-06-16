33db4f726a625cb212d67e3e3528d0eb8cb19c9a
nunks500 <joeldinisei1@gmail.com>
2018-06-16 20:50:38 +0100

    initial commit3

--- server/server.js
+++ server/server.js
@@ -4,10 +4,11 @@ const path = require('path');
 const app = express();
 var bodyParser = require('body-parser');
 
-require('./routes/index')(app);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./routes/index')(app);
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
